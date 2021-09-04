a253778ddaa4647584d0acfe23e9bd99ab063937
caojoshua <cao.joshua@yahoo.com>
2021-09-04 15:04:18 -0700

    Assignment 1 Phase 3: Shining C.

--- tut/1-blinky/phase3/blinky.c
+++ tut/1-blinky/phase3/blinky.c
@@ -15,6 +15,11 @@ static void spin_sleep_ms(unsigned int ms) {
 }
 
 int kmain(void) {
-  // FIXME: STEP 1: Set GPIO Pin 16 as output.
-  // FIXME: STEP 2: Continuously set and clear GPIO 16.
+  *GPIO_FSEL1 = 1 << 18;
+  while (1) {
+    *GPIO_SET0 = 1 << 16;
+    spin_sleep_ms(500);
+    *GPIO_CLR0 = 1 << 16;
+    spin_sleep_ms(500);
+  }
 }
