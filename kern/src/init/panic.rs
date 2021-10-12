a8fb93c97089fb0d93294cb2773da49fb0e1a21a
caojoshua <cao.joshua@yahoo.com>
2021-10-11 20:24:15 -0700

    panic handler.

--- kern/src/init/panic.rs
+++ kern/src/init/panic.rs
@@ -1,6 +1,14 @@
+use core::time::Duration;
 use core::panic::PanicInfo;
+use crate::console::kprintln;
+use pi::timer::spin_sleep;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    for _ in 0 .. 5 {
+        kprintln!("{}", _info);
+        spin_sleep(Duration::from_secs(1));
+    }
+
     loop {}
 }
