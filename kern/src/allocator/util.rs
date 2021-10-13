64b8c000e0a2ea29949f7e6e6eb33ba22fe00bac
caojoshua <cao.joshua@yahoo.com>
2021-10-12 20:46:26 -0700

    align utilities panic when `align` is not power of two.

--- kern/src/allocator/util.rs
+++ kern/src/allocator/util.rs
@@ -6,9 +6,7 @@
 ///
 /// Panics if `align` is not a power of 2.
 pub fn align_down(addr: usize, align: usize) -> usize {
-    if align % 2 != 0 {
-        panic!()
-    }
+    verify_power_of_two(align);
     addr - addr % align
 }
 
@@ -21,13 +19,30 @@ pub fn align_down(addr: usize, align: usize) -> usize {
 /// Panics if `align` is not a power of 2
 /// or aligning up overflows the address.
 pub fn align_up(addr: usize, align: usize) -> usize {
-    if align % 2 != 0 {
-        panic!()
-    }
-
+    verify_power_of_two(align);
     if addr % align == 0 {
         addr
     } else {
         addr + align - addr % align
+    }
+}
+
+/// Returns true if `align` is a power of 2. false otherwise.
+///
+/// This function uses the fact that a number `x` that is a power of
+/// 2 has a single bit, and a number `x - 1` is full of 1 bits to the
+/// right of the single set bit in `x`. Therefore, `x & (x - 1)`
+/// must be zero. If `y` is not a power of 2, `y` and `y - 1`
+/// must share a single bit, and `y & (y - 1)` must not be zero.
+pub fn is_power_of_two(align: usize) -> bool {
+    align & (align - 1) == 0
+}
+
+/// Verifies that `align` is a power of 2. Panics otherwise.r!
+///
+/// # Panics
+fn verify_power_of_two(align: usize) {
+    if !is_power_of_two(align) {
+        panic!("`{}` is not a power of 2")
     }
 }
