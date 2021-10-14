6fd4aeae49a606edd1c92a32abf16e2f772c034d
caojoshua <cao.joshua@yahoo.com>
2021-10-14 00:15:17 -0700

    Bin allocator. Passing all `make test`.

--- kern/src/allocator/bin.rs
+++ kern/src/allocator/bin.rs
@@ -1,48 +1,136 @@
 use core::alloc::Layout;
-use core::fmt;
 use core::ptr;
 
 use crate::allocator::linked_list::LinkedList;
 use crate::allocator::util::*;
 use crate::allocator::LocalAlloc;
 
+use crate::console::kprintln;
+
 /// A simple allocator that allocates based on size classes.
 ///   bin 0 (2^3 bytes)    : handles allocations in (0, 2^3]
 ///   bin 1 (2^4 bytes)    : handles allocations in (2^3, 2^4]
 ///   ...
-///   bin 29 (2^22 bytes): handles allocations in (2^31, 2^32]
+///   bin 29 (2^32 bytes): handles allocations in (2^31, 2^32]
 ///   
 ///   map_to_bin(size) -> k
 ///   
 
-const num_bins: usize = 30;
+/// Main points:
+/// 1. We have 11 bins, allowing allocations from 2^3 to 2^13, just
+/// enough to pass test cases. No plans to suport more unless
+/// there are use cases in the kernel.
+/// 2. On initialization, we align up to the largest size class we
+/// support (some testcases had < 8192 bytes). Then we keep on
+/// filling in memory with the largest class size that we can
+/// depending on how much memory is left.
+/// 3. If the largest size class we can support is N, than the
+/// largest alignment we guarentee to support is N. Since we align
+/// by N in the beginning, we can guarentee alignment for all other
+/// class sizes, because of x is aligned to N, then x is also
+/// aligned to `N >> m` for any m.
+/// 4. On allocation, we attempt to allocate in the bin with
+/// the smallest size class. If there are no bins with blocks
+/// are available, we sequentially check bins with larger size
+/// classes. We attempt to split free memory in `split_memory_block` to
+/// decrease external fragmentation.
+///
+/// Areas of improvement:
+/// 1. Use a backup allocator. As we break up large memory blocks
+/// and free them, we cannot coalesce the free memory blocks.
+/// We also cap out the max allocation size. A backup allocator
+/// will allow large allocations that this allocator might not
+/// support.
+/// 2. Decrease alignment requirements. If we currently support
+/// a max size class of N, we also support alignment up to N.
+/// Since we align in the beginning up to N, in the worst case
+/// we do not use `N - 1` bytes, which causes issues in test
+/// cases with small memory totols. In practice, we probably
+/// only need to align up to 16 anyway. It should be doable
+/// to decouple max size class and alignment.
 
+/// set NUM_BINS to the mimimum that will past test cases. We
+/// can increase this later, but any value >= 13 fails test cases.
+const NUM_BINS: usize = 11;
+const BIN_SMALLEST_K: usize = 3;
+const MIN_SIZE_CLASS: usize = 1 << BIN_SMALLEST_K;
+const MAX_SIZE_CLASS: usize = 1 << (NUM_BINS + BIN_SMALLEST_K - 1);
+
+#[derive(Debug)]
 pub struct Allocator {
     /// `bins` is a array of `num_bins` LinkedLists. `bins[k]`
-    /// contains allocations of size 2^(k+3), allowing allocations
-    /// of max size 2^32.
-    bins: [LinkedList; num_bins],
+    /// contains allocations of size 2^(k+3)
+    bins: [LinkedList; NUM_BINS],
 }
 
 impl Allocator {
     /// Creates a new bin allocator that will allocate memory from the region
     /// starting at address `start` and ending at address `end`.
     pub fn new(start: usize, end: usize) -> Allocator {
-        let bin_size = (end - start) / num_bins;
-        let mut bins = [LinkedList::new(); num_bins];
-        // TODO: compute `addr` through iterations
-        let mut addr = start;
+        let mut bins = [LinkedList::new(); NUM_BINS];
 
-        for bin_idx in 0 .. bin_size {
-            let site_size = (2 as usize).pow((bin_idx + 3) as u32);
-            let bin_num_sites = bin_size / site_size; 
-            for _ in 0 .. bin_num_sites {
+        let mut size_class = MAX_SIZE_CLASS;
+        let mut addr;
+
+        // Find the start address that is aligned with the largest
+        // size class.
+        loop {
+            let candidate_start = align_up(start, size_class);
+            if candidate_start + size_class < end {
+                addr = candidate_start;
+                break;
+            }
+            size_class /= 2;
+        }
+
+        // Fill bins with the largest class size possible.
+        for bin_idx in (0 .. NUM_BINS).rev() {
+            while addr + size_class < end {
                 unsafe { bins[bin_idx].push(addr as *mut usize); }
-                addr += site_size;
+                addr += size_class;
             }
         }
 
         Allocator { bins }
+    }
+
+    /// return the bin index with the smalles size class that `size` would
+    /// fit into.
+    fn map_to_bin(&self, size: usize) -> Option<usize> {
+        if size > MAX_SIZE_CLASS {
+            return None
+        }
+
+        let mut bin_size = MIN_SIZE_CLASS;
+        let mut bin_idx = 0;
+        while size > bin_size {
+            bin_size *= 2;
+            bin_idx += 1;
+        }
+
+        Some(bin_idx)
+    }
+
+    /// return the bin class size of `bin_idx`
+    fn map_to_bin_class_size(&self, bin_idx: usize) -> usize {
+        (2 as usize).pow((BIN_SMALLEST_K + bin_idx) as u32)
+    }
+
+    /// When ptr is being allocated into a bin that is not the smallest possible
+    /// class size, this function will split unused memory blocks into free blocks.
+    unsafe fn split_memory_block(&mut self, ptr: usize, ptr_size: usize, alloc_size: usize) {
+        if alloc_size >= ptr_size {
+            return;
+        }
+
+        let half_size = ptr_size / 2;
+        match self.map_to_bin(ptr_size / 2) {
+            Some(bin_idx) => {
+                self.bins[bin_idx].push((ptr + half_size) as *mut usize);
+                self.split_memory_block(ptr, half_size, alloc_size);
+            }
+            _ => (),
+        };
     }
 }
 
@@ -72,8 +160,33 @@ impl LocalAlloc for Allocator {
         if layout.size() <= 0 || !is_power_of_two(layout.align()) {
             return ptr::null_mut();
         }
-        
-        ptr::null_mut()
+
+        let mut bin_idx = match self.map_to_bin(layout.size()) {
+            Some(bin_idx) => bin_idx,
+            None => return ptr::null_mut(),
+        };
+
+        let mut bin_class_size = self.map_to_bin_class_size(bin_idx);
+        let original_bin_size = bin_class_size;
+        loop {
+            // Look for a free memory block in this bin.
+            for bin in self.bins[bin_idx].iter_mut() {
+                if bin.value() as usize % layout.align() == 0 {
+                    let ptr = bin.pop();
+                    self.split_memory_block(ptr as usize, bin_class_size, original_bin_size);
+                    return ptr as *mut u8;
+                }
+            }
+
+            // Advance to the next bin the find free memory blocks.
+            bin_idx += 1;
+
+            // Unable to find free block. Return null.
+            if bin_idx == NUM_BINS {
+                return ptr::null_mut();
+            }
+            bin_class_size *= 2;
+        }
     }
 
     /// Deallocates the memory referenced by `ptr`.
@@ -90,8 +203,10 @@ impl LocalAlloc for Allocator {
     /// Parameters not meeting these conditions may result in undefined
     /// behavior.
     unsafe fn dealloc(&mut self, ptr: *mut u8, layout: Layout) {
-        unimplemented!("bin allocator")
+        let bin_idx = match self.map_to_bin(layout.size()) {
+            Some(bin_idx) => bin_idx,
+            None => return,
+        };
+        self.bins[bin_idx].push(ptr as *mut usize);
     }
 }
-
-// FIXME: Implement `Debug` for `Allocator`.
