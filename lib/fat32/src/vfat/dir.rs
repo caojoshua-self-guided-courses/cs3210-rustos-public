6292baeb906923e9290759b3797b46916fadc1aa
caojoshua <cao.joshua@yahoo.com>
2021-11-11 00:00:28 -0800

    Dir::find and logic for long filenames.
    Also some formatting and commenting.

--- lib/fat32/src/vfat/dir.rs
+++ lib/fat32/src/vfat/dir.rs
@@ -15,6 +15,7 @@ use crate::vfat::{Attributes, Date, Metadata, Time, Timestamp};
 use crate::vfat::{Cluster, Entry, FatEntry, File, VFatHandle};
 
 const LONG_FILENAME_MARKER: u8 = 0xF;
+const LONG_FILENAME_MAX_CHARS: u8 = 13;
 
 #[derive(Clone, Debug)]
 pub struct Dir<HANDLE: VFatHandle> {
@@ -90,7 +91,26 @@ impl<HANDLE: VFatHandle> Dir<HANDLE> {
     /// If `name` contains invalid UTF-8 characters, an error of `InvalidInput`
     /// is returned.
     pub fn find<P: AsRef<OsStr>>(&self, name: P) -> io::Result<Entry<HANDLE>> {
-        unimplemented!("Dir::find()")
+        let name = match name.as_ref().to_str() {
+            Some(str) => str,
+            None => {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "`name` contains invalid UTF-8 characters",
+                ))
+            }
+        };
+
+        for entry in traits::Dir::entries(self)? {
+            if self.name == name {
+                return Ok(entry);
+            }
+        }
+
+        Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            format!("`{}` not found in `{}`", name, self.name),
+        ))
     }
 }
 
@@ -119,33 +139,26 @@ impl<HANDLE: VFatHandle> traits::Dir for Dir<HANDLE> {
             Ok(())
         })?;
 
-        fn trim(bytes: &[u8]) -> Vec<u8> {
-            let mut vec: Vec<u8> = Vec::new();
-            for &byte in bytes {
-                if byte == 0x00 || byte == 0xFF {
-                    break;
-                }
-                vec.push(byte);
-            }
-            vec
-        }
-
         let mut curr = 0;
         let mut entries: Vec<Entry<HANDLE>> = Vec::new();
 
         while curr < entries.len() {
-            let mut unknown_dir_entry: VFatUnknownDirEntry = unsafe {
-                vfat_entries[curr].unknown
-            };
+            let mut unknown_dir_entry: VFatUnknownDirEntry = unsafe { vfat_entries[curr].unknown };
 
             // Compute the long file name if it exists.
-            // TODO: long filename entries are not necessarily in order,
-            // need to check sequence number
             let mut long_name: Vec<u16> = Vec::new();
             while unknown_dir_entry.attributes.0 & LONG_FILENAME_MARKER == LONG_FILENAME_MARKER {
-                let long_filename = unsafe {
-                    vfat_entries[curr].long_filename
-                };
+                let long_filename = unsafe { vfat_entries[curr].long_filename };
+
+                // Compute the index for this long file name entry
+                let lfn_sequence_number = long_filename.sequence_number & 0b1111;
+                let mut lfn_idx = lfn_sequence_number * LONG_FILENAME_MAX_CHARS;
+
+                // Resize long_name if the new sequnce number is the largest seen so far.
+                let diff = lfn_idx as i32 - long_name.len() as i32;
+                if diff > 0 {
+                    long_name.resize(long_name.len() + diff as usize, 0);
+                }
 
                 let char_sets: [&[u16]; 3] = [
                     &long_filename.name_first,
@@ -153,56 +166,61 @@ impl<HANDLE: VFatHandle> traits::Dir for Dir<HANDLE> {
                     &long_filename.name_third,
                 ];
 
+                // Insert character into long_filename
                 'outer: for char_set in char_sets.iter() {
                     for character in char_set.iter() {
-                        let character = *character;
-                        if character == 0x00 || character == 0xFF {
-                            break 'outer;
-                        }
-                        long_name.push(character);
+                        long_name[lfn_idx as usize] = *character;
+                        lfn_idx += 1;
                     }
                 }
 
-                unknown_dir_entry = unsafe { vfat_entries[curr].unknown };
                 curr += 1;
+                unknown_dir_entry = unsafe { vfat_entries[curr].unknown };
             }
 
+            let regular = unsafe { vfat_entries[curr].regular };
             curr += 1;
 
-            let regular = unsafe {
-                vfat_entries[curr].regular
-            };
-
-
+            // Helper to return a Vector with characters up until
+            // terminating characters
+            fn trim<T: Copy + Into<u16>>(bytes: &[T]) -> Vec<T> {
+                let mut vec: Vec<T> = Vec::new();
+                for &byte in bytes {
+                    if byte.into() == 0x00 || byte.into() == 0xFF {
+                        break;
+                    }
+                    vec.push(byte);
+                }
+                vec
+            }
+            let long_name = trim(long_name.as_slice());
             let short_name = trim(&regular.name);
             let extension = trim(&regular.extension);
 
-            let name = String::from_utf16(&long_name.as_slice()).unwrap() +
-                from_utf8(short_name.as_slice()).unwrap() +
-                "." + from_utf8(extension.as_slice()).unwrap();
+            let name = String::from_utf16(&long_name.as_slice()).unwrap()
+                + from_utf8(short_name.as_slice()).unwrap()
+                + "."
+                + from_utf8(extension.as_slice()).unwrap();
 
-            let entry_cluster = (regular.first_cluster_high_16 as u32) << 16 + regular.first_cluster_low_16;
+            let entry_cluster =
+                (regular.first_cluster_high_16 as u32) << 16 + regular.first_cluster_low_16;
 
             let is_directory = regular.attributes.0 & 0x10 != 0;
             entries.push(if is_directory {
                 // TODO: other fields ie. date created, date modified
-                Entry::Dir(
-                    Dir {
-                        vfat: self.vfat.clone(),
-                        cluster: Cluster { 0: entry_cluster },
-                        name
-                    }
-                )
+                Entry::Dir(Dir {
+                    vfat: self.vfat.clone(),
+                    cluster: Cluster { 0: entry_cluster },
+                    name,
+                })
             } else {
-                Entry::File(
-                    File {
-                        vfat: self.vfat.clone(),
-                    }
-                )
+                Entry::File(File {
+                    vfat: self.vfat.clone(),
+                })
             });
         }
 
-        Ok(EntryIterator{
+        Ok(EntryIterator {
             phantom: PhantomData,
             curr: 0,
             entries,
